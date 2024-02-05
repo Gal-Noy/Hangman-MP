28aec1567357faefe0d0da3637f1b9021508d8a2
Gal Noy <109943831+Gal-Noy@users.noreply.github.com>
2024-02-05 18:14:28 +0200

    changed port number

--- server/src/index.js
+++ server/src/index.js
@@ -6,5 +6,9 @@ const app = express();
 server(app);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is listening on port 3000");
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
